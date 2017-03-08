8e3b0bd8e7fc5faae0f910a80964ccc8a9df6371
LAPTOP-16A9ELL1\Photography <aveendramanoj777@gmail.com>
2017-03-08 10:08:00 +0400

    Project initializing.

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+// IIFE - Immediately Invoked Function Expression
+(function($, window, document) {
+	"use strict";
+
+    // The $ is now locally scoped 
+
+
+    // Listen for the jQuery ready event on the document
+    $(function() {
+
+		// Scripts goes here
+
+    });
+
+    // The rest of the code goes here!
+
+}(window.jQuery, window, document));
+// The global jQuery object is passed as a parameter
+    
+   
